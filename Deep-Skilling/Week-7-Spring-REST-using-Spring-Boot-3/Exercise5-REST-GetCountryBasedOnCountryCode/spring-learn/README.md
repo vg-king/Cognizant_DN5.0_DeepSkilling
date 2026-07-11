25a631031611dc14c9edc5d2e8f8624f160b5932
vg-king <vg3772285@gmail.com>
2026-07-11 15:41:09 +0530

    Week 7: Complete Exercise 5 - Get Country By Country Code

---
+++ Deep-Skilling/Week-7-Spring-REST-using-Spring-Boot-3/Exercise5-REST-GetCountryBasedOnCountryCode/spring-learn/README.md
@@ -0,0 +1,30 @@
+# Exercise 5 - REST Get Country By Code
+
+## Objective
+
+Return country details based on country code.
+
+## Endpoint
+
+GET /countries/{code}
+
+## Example
+
+GET /countries/in
+
+## Response
+
+```json
+{
+  "code":"IN",
+  "name":"India"
+}
+```
+
+## Features
+
+- Spring XML Configuration
+- REST API
+- Service Layer
+- PathVariable
+- Case Insensitive Search
---
+++ Deep-Skilling/Week-7-Spring-REST-using-Spring-Boot-3/Exercise5-REST-GetCountryBasedOnCountryCode/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -0,0 +1,27 @@
+package com.cognizant.springlearn.service;
+
+import com.cognizant.springlearn.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countries =
+                (List<Country>) context.getBean("countryList");
+
+        return countries.stream()
+                .filter(country ->
+                        country.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+    }
+}
